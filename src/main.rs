d3b91f0a0f146ce125090489921aa19c9b7fa1d7
Bohdan Shkaran <bogdan.shkaran@gmail.com>
2026-02-19 13:05:15 +0200

    feat: base setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Extension, Router, routing::get};
+use dotenvy::dotenv;
+use sqlx::postgres::PgPoolOptions;
+use tracing::{Level, info};
+use tracing_subscriber;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    tracing_subscriber::fmt().with_max_level(Level::INFO).init();
+
+    dotenv().ok();
+    let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPoolOptions::new().connect(&url).await?;
+    info!("Connected to the database!");
+
+    let app = Router::new().route("/", get(root)).layer(Extension(pool));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
+    info!("Server is running on http://localhost:5000");
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+}
+
+async fn root() -> &'static str {
+    "Hello, world!"
 }
